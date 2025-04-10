ea6ca1881aec32a701d20c6799dc7e2d100c7884
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-04-10 09:22:52 -0400

    Add categoria class

---
+++ Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperColmadoDennys.Models;
+
+public class Categoria
+{
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required(ErrorMessage = "No puede estar Vacio")]
+    [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "El Nombre debe contener solo letras.")]
+    public string? Nombre { get; set; }
+}
