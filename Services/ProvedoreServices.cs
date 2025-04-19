a224f062e1597c479853f287a549bb3c5fca524d
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-04-19 13:46:26 -0400

    Add ProvedoreServices class for database management

---
+++ Services/ProvedoreServices.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperColmadoDennys.Data;
+using SuperColmadoDennys.Models;
+using System.Linq.Expressions;
+
+namespace SuperColmadoDennys.Services;
+
+public class ProvedoreServices(IDbContextFactory<ApplicationDbContext> DbFactory)
+{
+    public async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Provedores.AnyAsync(p => p.ProvedorId == id);
+    }
+    private async Task<bool> Insertar(Provedores provedor)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Provedores.Add(provedor);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    private async Task<bool> Modificar(Provedores provedor)
+    {
+        await using var _context = await DbFactory.CreateDbContextAsync();
+        var local = _context.Provedores.Local
+            .FirstOrDefault(p => p.ProvedorId == provedor.ProvedorId);
+        _context.Entry(provedor).State = EntityState.Modified;
+        return await _context.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> Guardar(Provedores provedore)
+    {
+        if (!await Existe(provedore.ProvedorId))
+            return await Insertar(provedore);
+        else
+            return await Modificar(provedore);
+    }
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var Provedor = await contexto.Provedores
+            .Where(p => p.ProvedorId == id).ExecuteDeleteAsync();
+        return Provedor > 0;
+    }
+    public async Task<Provedores?> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Provedores.AsNoTracking().
+            FirstOrDefaultAsync(p => p.ProvedorId == id);
+    }
+    public async Task<Provedores?> BuscarNombres(string nombre)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Provedores.AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Nombre == nombre);
+    }
+    public async Task<List<Provedores>> Listar(Expression<Func<Provedores, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Provedores.AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
