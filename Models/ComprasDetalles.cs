1ec07823b04946256dd24921c21e63ba9dc684bb
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-04-19 14:32:42 -0400

    Add ComprasDetalles class with foreign key relationship

---
+++ Models/ComprasDetalles.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperColmadoDennys.Models;
+
+public class ComprasDetalles
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int CompraId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+    public int ProductoId { get; set; }
+
+    [ForeignKey("ProductoId")]
+    public Productos? Productos { get; set; }
+}
