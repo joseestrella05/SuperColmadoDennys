d0b30c37e73850d9bd9d971a98b43c51eaec2a0f
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-04-20 15:06:05 -0400

    Add user profile properties to ApplicationUser class

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -5,5 +5,13 @@ namespace SuperColmadoDennys.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public string? Cedula { get; set; }
+
+    public string? NickName { get; set; }
+
 }
 
