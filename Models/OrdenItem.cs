48de1bd36519cfc577bffa138fc90f4e736314f9
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-04-21 12:56:40 -0400

    Add OrdenItem class with validation and relationships

---
+++ Models/OrdenItem.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperColmadoDennys.Models;
+
+public class OrdenItem
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("Ordenes")]
+    public int OrdenId { get; set; }
+
+    [ForeignKey("Productos")]
+    public int ProductoId { get; set; }
+
+    [Required(ErrorMessage = "Un Producto es requerido")]
+    public Productos? Producto { get; set; }
+
+    [Required(ErrorMessage = "Indique el Precio")]
+    [Range(0.01, 1000000000, ErrorMessage = "El Precio debe estar 0.01 y 1000000000")]
+    public float Precio { get; set; }
+
+    [Required(ErrorMessage = "Es requerido")]
+    public int Cantidad { get; set; }
+
+    public Ordenes? Orden { get; set; }
+
+}
